d7d302bbec309c2903bba0abd071549c2034d963
ALSRUD29 <dev.mnkg@gmail.com>
2023-01-10 19:49:29 +0900

    chore : eslint, prettier 설정

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 100, 
+    singleQuote: true, 
+    arrowParens: "avoid", 
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: true,
+    endOfLine: 'auto',
+    "useTabs": false,
+  };
